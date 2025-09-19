a4de63e196b5e990137b4df7f320f4d1a24dcc73
David Brownlee <abs@absd.org>
2025-09-19 23:15:57 +0100

    Note this needs a minSdk of 26

--- datawedge_flutter/android/app/build.gradle.kts
+++ datawedge_flutter/android/app/build.gradle.kts
@@ -24,7 +24,8 @@ android {
         applicationId = "com.darryncampbell.datawedgeflutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // @@ registerReceiver() needs minSdk of 26
+        minSdk = 26 // flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
